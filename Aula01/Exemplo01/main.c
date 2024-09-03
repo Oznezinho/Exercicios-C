70d41cb47fa6df27ab1106d15231ee37c6200fef
Enzo Maçaneiro <127849398+Oznezinho@users.noreply.github.com>
2024-09-03 11:14:31 -0300

    Add files via upload

---
+++ Aula01/Exemplo01/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Este é o primeiro exemplo de linguagem em "C"
+int main(void) {
+
+  // Exibi as informações no console
+  printf("Meu Primeiro Trabalho em C\n");
+  printf("Meu Nome é: Enzo\n");
+  // Sucesso ao compilar o codigo
+  return 0;
+}
